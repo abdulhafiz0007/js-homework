1f49e3b5439488ee0bcdeb53f94c6a8f56e91b7f
abdulhafiz0007 <150119146+abdulhafiz0007@users.noreply.github.com>
2023-12-22 21:35:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var userName = prompt("what is your name");
+var familyName = prompt("What is your family name");
+var age = prompt("How old are you");
+var emailAccount = prompt("Enter your email");
+var dollorQuantity = prompt("Enter the quantity of dollor");
+var currencyExchange = dollorQuantity * 12400;
+
+console.log(`Your name is: ${userName} \n Your family name is: ${familyName} \n Your age is: ${age} \n Your email account is: ${emailAccount} \n ${dollorQuantity}$ would be ${currencyExchange} in sum`);
+
+
+
+
+
